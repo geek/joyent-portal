f04ae8dab7f58d06ccc40e8a6fc2ca71b940c9b2
Sérgio Ramos <mail@sergioramos.me>
2017-06-09 05:26:25 +0100

    feat(joyent-cp-frontend): create dg initial implementation

--- packages/cp-gql-mock-server/src/resolvers.js
+++ packages/cp-gql-mock-server/src/resolvers.js
@@ -1,3 +1,8 @@
+const { v4: uuid } = require('uuid');
+const paramCase = require('param-case');
+const camelCase = require('camel-case');
+const yaml = require('js-yaml');
+
 const {
   datacenter,
   portal,
@@ -41,6 +46,43 @@ const getPortal = () =>
 const getServices = query =>
   services.filter(find(query)).map(getDeploymentGroupServices);
 
+const createDeploymentGroup = ({ name }) => {
+  const dg = {
+    id: uuid(),
+    slug: paramCase(name),
+    name
+  };
+
+  deploymentGroups.push(dg);
+
+  return Promise.resolve(dg);
+};
+
+const createServicesFromManifest = ({ deploymentGroupId, raw }) => {
+  const manifest = yaml.safeLoad(raw);
+
+  Object.keys(manifest).forEach(name => {
+    const service = {
+      id: uuid(),
+      deploymentGroup: deploymentGroupId,
+      slug: paramCase(name),
+      name
+    };
+
+    const instance = {
+      id: uuid(),
+      name: camelCase(`${service.slug}_01`),
+      service: service.id,
+      deploymentGroup: deploymentGroupId
+    };
+
+    services.push(service);
+    instances.push(instance);
+  });
+
+  return Promise.resolve(undefined);
+};
+
 module.exports = {
   portal: (options, request, fn) => fn(null, getPortal()),
   deploymentGroups: (options, request, fn) =>
@@ -48,5 +90,12 @@ module.exports = {
   deploymentGroup: (options, request, fn) =>
     fn(null, getDeploymentGroups(options).shift()),
   services: (options, request, fn) => fn(null, getServices()),
-  service: (options, request, fn) => fn(null, getServices(options).shift())
+  service: (options, request, fn) => fn(null, getServices(options).shift()),
+  createDeploymentGroup,
+  provisionManifest: options =>
+    createServicesFromManifest(options).then(() => ({
+      id: uuid(),
+      type: options.type,
+      format: options.format
+    }))
 };
